9ca659f9b6443274449f7c9624f0edfabeefcc70
starcev_misha <starcev_misha@mail.ru>
2019-03-23 18:42:07 +0500

    Добвил парсинг аргументов

--- biosasci.asm
+++ biosasci.asm
@@ -1,14 +1,90 @@
 model tiny
+.data 
+    help_msg    	db "This programm prints ASCII table on the screen", 0Dh, 0Ah, "/m - specify, which mode to use (default 2):", 0Dh, 0Ah, "     0, 1 - 16 colors, 40x25 (25 rows with 40 symbols) with gray\without", 0Dh, 0Ah,"     2, 3 - 16 colors, 80x25 with gray\without", 0Dh, 0Ah, "/p - specify, on which page to print (default 0):", 0Dh, 0Ah, "     0-7 for modes 0 and 1.", 0Dh, 0Ah, "     0-3 for modes 2 and 3.", 0Dh, 0Ah, "/? - this help.", 0Dh, 0Ah, "$"
+    SLASH   = '/'
+    HELP    = '?'
+    MODE    = 'm'
+    PAGE    = 'p'
+    mode_num		db 00000010b
+    page_num		db 0
+
 .code
 org 100h
-
 start:
+
+    buffer      db 30 DUP (0)
+    mov cl, ds:[0080h]  ; CX: number of bytes to write
+    mov di, 81h
+
+
+args_loop:   
+    call read_next_arg
+    
+    mov bl, 0
+	cmp bl, [buffer]
+    je prog_start
+    
+    mov bl, SLASH
+	cmp bl, [buffer]
+    jne help_message_print
+
+    mov bl, HELP      
+	cmp bl, [buffer+1]
+    je help_message_print
+
+    mov bl, MODE      
+	cmp bl, [buffer+1]
+    je mode_choose
+
+    mov bl, PAGE      
+	cmp bl, [buffer+1]
+    je page_choose
+
+    jmp exit
+
+
+mode_choose:
+    call read_next_arg
+    mov bl, [buffer]
+    sub bl, 30h
+    mov [mode_num], bl
+    jmp args_loop
+page_choose:
+    call read_next_arg
+    mov bl, [buffer]
+    sub bl, 30h
+    mov [mode_num], bl
+    jmp args_loop
+
+
+help_message_print:
+    mov dx, offset help_msg
+    mov ah, 9
+    int 21h
+    
+exit:
+    mov ax,4C00h
+    int 21h 
+
+
+prog_start:
+    mov ah, 05h
+	mov al, byte ptr page_num
+	int 10h
+
     mov ah, 0
-    mov al, 00000011b ;Первый бит = очистиь экран
+    mov al,byte ptr mode_num ;Первый бит = очистиь экран
     int 10h
 
     xor dx, dx
+    and al, 01111111b
+    cmp al,2
+    jl small_screen
     mov dl, (80-32)/2
+    jmp big_screen
+small_screen:
+    mov dl, (40-32)/2
+big_screen:
     mov dh, (25-16)/2
     mov ah, 2
     int 10h
@@ -20,7 +96,7 @@ start:
     mov cx, 1   ; число повторений
     mov bl, 000000000b ; арибут
 
-loop1:    
+main_loop:    
     mov bl, 000001111b
     cmp di, 1
     jne next1
@@ -88,9 +164,51 @@ skip_last_space:
     pop ax
 check:
     dec si
-    jnz loop1
-
+    jnz main_loop
+    
     mov ax,4C00h
     int 21h 
+
+read_next_arg proc
+    ; читает строку из di, пропускает пробелы
+    ; и  помещает первое значение после пробелов в buffer
+    cld
     
+    push di
+    push cx
+    mov al, 0
+    mov di, offset buffer
+    xor cx, cx
+    mov cx, 30
+    rep stosb
+    pop cx
+    pop di
+
+    mov al, ' '
+    repe scasb
+    jz stop_loop1
+
+    dec di
+    inc cx
+
+    mov si, di
+    mov di, offset buffer
+    loop1:
+    mov al, [si]
+    cmp al, 20h
+    je stop_loop1
+    cmp cl, 0h
+    je stop_loop1
+    inc si
+    
+    stosb
+    dec cx
+    jmp loop1
+    stop_loop1:
+    mov al, '$'
+    stosb
+    mov di, si ; возвращаем обратно
+    ret
+read_next_arg endp
+
 end start
