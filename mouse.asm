89bb66f45bae98c502d02371e9399bd62732ef23
starcev_misha <starcev_misha@mail.ru>
2019-05-03 18:58:50 +0500

    меняет цвета по нажатию правой кнопки

--- mouse.asm
+++ mouse.asm
@@ -2,16 +2,23 @@ model tiny
 locals
 
 .data
+old     dd  0                   ; адрес старого обработчика
     bold    dw 3
     width   dw 301
     height  dw 101
     startx  dw 100
     starty  dw 100
-    color   db 10
+    newx    dw 100
+    newy    dw 100
+    color   db 1
     
     circle_x dw 100
     circle_y dw 100
+    new_circle_x dw 100
+    new_circle_y dw 100
     circle_rad   db 10
+    is_exit db 0
+    is_repaint db 1
 
 .code
 org 100h
@@ -20,45 +27,123 @@ start:
     mov     AL,10h  ;выбор режима 10h
     int     10h     ;обращение к видео-BIOS
 
+
+    mov         ax,0         ; инициализировать мышь
+    int         33h
+    mov         ax,1         ; показать курсор мыши
+    int         33h
+    
+    mov         ax, 000Ch   ; установить обработчик событий мыши
+    mov         cx, 001000b ; событие - левая кнопка
+    mov         dx, offset mouse_handler
+    int         33h
+    
+    call repaint
+    
+    mov         ah,0         ; ожидание нажатия любой клавиши
+    int         16h
+
+    jmp exit
+
+mouse_handler proc
+    ; test ax, 00001b ; перемещение
+    ; je move
+
+    cmp ax, 01000b ;правая кнопка мыши
+    je rmb
+    jmp ret1
+    ; move:
+    ;     jmp ret1
+    ;     ; mov new_circle_x, cx
+    ;     ; mov new_circle_y, dx
+    ;     ; call repaint
+
+    lmb_move:
+    rmb:  
+        push ax
+        mov al, color
+        inc al
+        and al, 01111b
+        mov color, al
+        pop ax
+        call repaint
+    ret1:
+        retf
+mouse_handler endp
+
+
+repaint:
+    mov         ax,2        ; спрятать
+    int         33h
+
+
+    
     mov cx, startx
     mov dx, starty
-    mov al, 10
     call draw_rectangle
 
-    mov al ,6
+    mov cx, startx
+    mov dx, starty
     call draw_circle
 
-    ; mov     AH,0ch  ;функция ch - изображение точки
-    ; mov     BH,0    ;выбор страницы 0
-    ; mov cx, 100
-    ; mov dx, 100
-    ; mov al , 6
-    ; int     10h     ;обращение к видео-BIOS
-
-    ; mov cx, 100
-    ; mov dx, 200
-    ; mov al , 6
-    ; int     10h     ;обращение к видео-BIOS
-
+    ; mov color, 15
     
-    ; mov cx, 400
-    ; mov dx, 200
-    ; mov al , 6
-    ; int     10h     ;обращение к видео-BIOS   
-    ;  mov cx, 400
-    ; mov dx, 100
-    ; mov al , 6
-    ; int     10h     ;обращение к видео-BIOS   
+    ; mov cx, startx
+    ; mov dx, starty
+    ; call draw_rectangle
+    ; mov cx, startx
+    ; mov dx, starty
+    ; call draw_circle
+
+    ; pop ax
+    ; mov color, al
     
-    ;  mov cx, 150
-    ; mov dx, 150
-    ; mov al , 15
-    ; int     10h     ;обращение к видео-BIOS   
+    ; mov cx, startx
+    ; mov dx, starty
+    ; call draw_rectangle
+    
+    ; mov cx, new_circle_x
+    ; mov dx, new_circle_y
+    ; mov al, color
+    ; xor al, 3
+    ; mov color, al
+    ; call draw_circle
+
+    ; mov bx, new_circle_x
+    ; mov circle_x, bx
+    ; mov bx, new_circle_y
+    ; mov circle_y, bx
+
+    mov         ax,1         ; показать курсор мыши
+    int         33h
+    
+    ret
+; repaint_rectangle:
+;     mov cx, startx
+;     mov dx, starty
+;     call draw_rectangle
+;     ret
+
+; repaint_circle:
 
 
+;     mov cx, new_circle_x
+;     mov dx, new_circle_y
+;     call draw_circle
 
+;     mov bx, new_circle_x
+;     mov circle_x, bx
+;     mov bx, new_circle_y
+;     mov circle_y, bx
 
+;     ret
 
+exit:
+    mov         ax,000Ch
+    mov         cx,0000h     ; удалить обработчик событий мыши
+    int         33h
+    mov         ax,3         ; текстовый режим
+    int         10h
     mov ax,4C00h
     int 21h
 
@@ -170,6 +255,7 @@ draw_circle proc
     mov al, color
     xor al, 3
     mov color, al
+    
     xor ax, ax  
     
     mov al, circle_rad
